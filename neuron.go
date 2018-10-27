9078d90d090cf9ffa51526eed5513483bcbad053
vnepveu <victor.nepveu@imt-atlantique.net>
2018-10-27 18:24:46 +0200

    Added updateWeights method

--- neuron.go
+++ neuron.go
@@ -2,25 +2,36 @@ package main
 
 
 type Perceptron struct {
+  // A Perceptron is determined by its size, weights and activation function
   size int
-  input_vector []float32{}
-  weights_vector []float32{}
+  weights []float32{}
   activation func(float32) float32
+  learningRate  float32
 }
-func (p Perceptron) updateWeights() {
-  new_weights = []float32
+func (p Perceptron) updateWeights(input []float32{}, target float32) (error){
+  // Update the weigth according to input, learningRate and activation
+  newWeights := []float32
+  if p.size != len(input) {
+    return fmt.Errorf("The input hasn't the right dimension")
+  }
+  sum := 0
+  for i, w_i := range(p.weights) {
+    sum += w_i * input[i]
+  }
+  output := p.activation(sum)
+  for i, w_i := range(p.weights) {
+    newWeight := w_i + p.learning_rate * (target - output) * input[i]
+    newWeights = append(newWeights, newWeight)
+  }
+  p.weights = newWeights
+  fmt.Println("Weights updated !")
 }
 
-func (p Perceptron) activation() {
-
-}
-func vectorDifference(u  []float32{}, v []float32{}) ([]float32, error){
-  if len(u) != len(v) {
-    return nil, fmt.Errorf("The two vectors aren't the same size")
+func activation(f float32) float32 {
+  // Implements the Rectified linear unit activation function
+  if f > 0 {
+    return f
+  } else {
+    return 0
   }
-  difference := []int
-  for i, val_i := range u {
-    difference = append(difference, val_i*v[i])
-  }
-  return difference
 }
